d8bc21bb6bae34c12314fc2ddb12a4039353fa3c
Hacker <your.email@gmail.com>
2016-06-17 20:28:37 +0800

    wode

---
+++ 门禁/门禁/Doors.h
@@ -0,0 +1,13 @@
+//
+//  Doors.h
+//  门禁
+//
+//  Created by qingyun on 16/4/18.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Doors : NSObject
+-(void)showDoors;
+@end
